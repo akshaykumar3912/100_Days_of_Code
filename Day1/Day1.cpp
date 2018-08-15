fc12955bab703cefb20000cd716b40af160d0b3b
Kaushl <kaushlendrapratap.9837@gmail.com>
2018-08-15 15:16:54 +0530

    Day1 completed

---
+++ Day1/Day1.cpp
@@ -0,0 +1,36 @@
+#include<iostream>
+using namespace std;
+
+class A
+{
+public:
+
+int a,b;
+int sum;
+void Input()
+{
+  cout<<"Enter two numbers you want to add"<<endl;
+  cin>>a>>b;
+}
+ void Add()
+ {
+   sum= (-(-a-b));
+ }
+
+void Display()
+{
+  cout<<"your sum is:";
+  cout<<sum;
+  }
+
+};
+
+int main()
+{
+  A obj;
+  obj.Input();
+  obj.Add();
+  obj.Display();
+  return 0;
+
+}
---
+++ Day1/InfoDay1.md
@@ -0,0 +1,4 @@
+# Day1_Challenge
+
+ Day 1 challenge was to write a program which is taking two integer input from user and finding sum of both the integers without using "+" operator.
+ It was easy,looking forward for more tough challenges.
